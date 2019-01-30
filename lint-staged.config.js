cd7e9e871648f9bef7b139db2b71a01bde78dcc1
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-01-30 19:15:02 +0100

    Add lint cmd to package.json Update lint-staged config to use it

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   hooks: {
     'package.json': ['prettier-package-json', 'git add'],
     'src/**/*.{ts,json}': [
-      'tslint --fix',
+      'yarn lint --fix',
       'prettier --write',
       'git add',
       'jest --findRelatedTests'
